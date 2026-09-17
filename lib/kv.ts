1cd54c13f9d62f340dfaedf5c0af5b5e536b081e
psicanalistaadilsoncosta-boop <psicanalistaadilsoncosta@gmail.com>
2026-09-17 09:29:19 -0300

    migrar

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,2 +1,6 @@
 import { Redis } from '@upstash/redis';
-export const kv = Redis.fromEnv();
+
+export const kv = new Redis({
+  url: process.env.informa_KV_REST_API_URL || process.env.KV_REST_API_URL || '',
+  token: process.env.informa_KV_REST_API_TOKEN || process.env.KV_REST_API_TOKEN || '',
+});
